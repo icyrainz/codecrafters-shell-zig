9b447477a806d16ee106a97d95922934d100f730
Tue Phan <icyrainz@gmail.com>
2024-12-21 01:35:03 -0800

    handle invalid commands

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,15 @@
 const std = @import("std");
 
+fn processCommand(input: []const u8) !void {
+    const stdout = std.io.getStdOut().writer();
+    if (std.mem.eql(u8, input, "hello")) {
+        try stdout.writeAll("Hello, world!\n");
+    } else {
+        try stdout.print("{s}: command not found\n", .{input});
+    }
+}
+
 pub fn main() !void {
-    // Uncomment this block to pass the first stage
     const stdout = std.io.getStdOut().writer();
     try stdout.print("$ ", .{});
 
@@ -9,6 +17,5 @@ pub fn main() !void {
     var buffer: [1024]u8 = undefined;
     const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
 
-    // TODO: Handle user input
-    _ = user_input;
+    try processCommand(user_input);
 }
