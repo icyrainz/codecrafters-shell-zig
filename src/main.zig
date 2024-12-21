be3b7f82a895c0a91a74df113716e71e97f4bc4f
Tue Phan <icyrainz@gmail.com>
2024-12-21 02:16:08 -0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -8,20 +8,41 @@ const ExitCode = enum(u8) {
     cont = 100,
 };
 
+var arena: std.heap.ArenaAllocator = undefined;
+var allocator: std.mem.Allocator = undefined;
+
 fn processCommand(input: []const u8) !ExitCode {
+    var commandItems = std.mem.tokenizeAny(u8, input, " ");
+    const cmd = commandItems.next() orelse return .cont;
+
+    var args = std.ArrayList([]const u8).init(allocator);
+    defer args.deinit();
+
+    while (commandItems.next()) |arg| {
+        try args.append(arg);
+    }
+
     const stdout = std.io.getStdOut().writer();
-    if (std.mem.eql(u8, input, "hello")) {
+    if (std.mem.eql(u8, cmd, "hello")) {
         try stdout.writeAll("Hello, world!\n");
         return .cont;
-    } else if (std.mem.eql(u8, input, "exit 0")) {
+    } else if (std.mem.eql(u8, cmd, "exit")) {
         return .success;
+    } else if (std.mem.eql(u8, cmd, "echo")) {
+        const echo_string = try std.mem.join(allocator, " ", args.items);
+        try stdout.print("{s}\n", .{echo_string});
+        return .cont;
     } else {
-        try stdout.print("{s}: command not found\n", .{input});
+        try stdout.print("{s}: command not found\n", .{cmd});
         return .cont;
     }
 }
 
 pub fn main() !void {
+    arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    allocator = arena.allocator();
+
     const stdout = std.io.getStdOut().writer();
     const stdin = std.io.getStdIn().reader();
 
